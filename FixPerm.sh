3f5f0ab4a5c0fd69f5ff3ce1077c9310b156f010
shunyi <M157q.tw@gmail.com>
2013-01-14 22:28:25 +0800

    add some comment for custome usage

--- FixPerm.sh
+++ FixPerm.sh
@@ -1,12 +1,12 @@
-#!/usr/local/bin/bash
+#!/usr/bin/env bash
 
-# Fix permission and owner/group                        
-                                                                                
-PROTECTED="/usr/home/gh40402/"                                            
-DIRS=`echo $PROTECTED`                       
-USER="gh40402"                                                                     
-GROUP="gh40402"                                                                   
-                                                                                
-chown -R $USER:$GROUP $DIRS                                                     
-find $DIRS -type d -exec chmod 700 {} \;                                        
-find $DIRS -type f -exec chmod 600 {} \;
+# Fix permission and owner/group
+
+HOME="/usr/home/gh40402/" #set path to home folder
+DIRS=`echo $HOME`
+USER="gh40402"
+GROUP="gh40402" # set to the right group name
+
+chown -R $USER:$GROUP $DIRS
+find $DIRS -type d -exec chmod 700 {} \; # set the folder default permission
+find $DIRS -type f -exec chmod 600 {} \; # set the file   default permission
