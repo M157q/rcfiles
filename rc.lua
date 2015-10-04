f6aef51f7d7c6cd93d06f6deffbd20fe877e0893
shunyi <M157q.tw@gmail.com>
2015-10-04 14:29:19 +0800

    Do not turn translucent when window is unfocus.

--- rc.lua
+++ rc.lua
@@ -660,7 +660,7 @@ client.connect_signal("manage", function (c, startup)
 end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus c.opacity = 1 end)
-client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal c.opacity = 0.9 end)
+client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal c.opacity = 1 end)
 -- }}}
 
 -- vim:set fdm=marker:
